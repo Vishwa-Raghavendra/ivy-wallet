ab68d473d4bd9d6eb230695f999d6168a8c2668f
Vishwa Raghavendra K S <vishwaraghavendraks@gmail.com>
2023-01-01 13:54:59 +0530

    BumpVersion: Minior

--- buildSrc/src/main/java/com/ivy/wallet/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/ivy/wallet/buildsrc/dependencies.kt
@@ -22,8 +22,8 @@ import org.gradle.kotlin.dsl.project
 
 object Project {
     //Version
-    const val versionName = "4.7.2"
-    const val versionCode = 133
+    const val versionName = "4.7.3"
+    const val versionCode = 134
 
     //Compile SDK & Build Tools
     const val compileSdkVersion = 31
